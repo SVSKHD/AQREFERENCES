f8056cbae57894e3696d64282aeb87d2e7f9cd22
hitesh sunkara <hiteshsunkara@eworldes.com>
2023-04-06 22:10:03 +0530

    new page components

---
+++ newclient/src/Pages/HomeIndex.js
@@ -0,0 +1,10 @@
+import HomeComponent from "../PageComponents/Home/Home"
+
+const HomeIndex = () =>{
+return(
+    <>
+    <HomeComponent/>
+    </>
+)
+}
+export default HomeIndex
--- newclient/src/index.js
+++ newclient/src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux'
+import {Provider} from "react-redux"
+import {composeWithDevTools} from "redux-devtools-extension"
+import rootReducer from "./reducers";
+
+// store
+const store = createStore(rootReducer, composeWithDevTools());
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+   <App />
+  </Provider>
+ 
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
