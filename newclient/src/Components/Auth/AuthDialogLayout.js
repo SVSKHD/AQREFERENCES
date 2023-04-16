ac82757ba25ee93d7d81dde94286a80c92bf5c04
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-04-16 21:57:53 +0530

    login and signup

--- newclient/src/Components/Auth/AuthDialogLayout.js
+++ newclient/src/Components/Auth/AuthDialogLayout.js
@@ -8,7 +8,9 @@ const AuthDialogLayout = (props) => {
         <div className="card">
           <div className="row g-0">
             <div className="col-md-4">
+              <div className="text-center">
               <img src={AQ} className="aq-image rounded-start" alt="..." />
+              </div>
             </div>
             <div className="col-md-8">
               <div className="card-body">{props.children}</div>
--- newclient/src/Components/Auth/LoginDialog.js
+++ newclient/src/Components/Auth/LoginDialog.js
@@ -1,9 +1,61 @@
-const LoginAuth = () =>{
-    return(
-        <>
+import { useState } from "react";
+import { FaGoogle } from "react-icons/fa";
+const LoginAuth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading , setLoading] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="container-fluid">
         <h1>Login</h1>
-        </>
-    )
-}
-export default LoginAuth
+        <hr />
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
 
+          <div className="row">
+            <div className="col-md-5">
+              <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="submit">
+                  Login
+                </button>
+              </div>
+            </div>
+            <div className="col-md-2 text-center">
+              <h4>or</h4>
+            </div>
+            <div className="col-md-5">
+              <div class="d-grid gap-2">
+                <button class="btn text-danger" type="submit">
+                  <FaGoogle size={25} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+export default LoginAuth;
