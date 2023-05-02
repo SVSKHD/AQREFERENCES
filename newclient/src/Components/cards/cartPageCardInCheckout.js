402cac712b89433de9bc3f36329c880bf795ad00
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-05-02 22:51:11 +0530

    new cart page layout

--- newclient/src/Components/cards/cartPageCardInCheckout.js
+++ newclient/src/Components/cards/cartPageCardInCheckout.js
@@ -1,13 +1,40 @@
-import { FaMinus, FaPlus } from "react-icons/fa"
-import { ButtonGroup, Button } from "react-bootstrap"
+import { FaMinus, FaPlus } from "react-icons/fa";
+import { ButtonGroup, Button } from "react-bootstrap";
 const CartPageComponent = (props) => {
-    return (
-        <>
-
-            <div className="card mb-3 shadow-lg">
+  return (
+    <>
+      <div class="card mb-3 shadow-lg">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              src={props.image}
+              class="img-fluid rounded-start"
+              alt={props.title}
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{props.title}</h5>
+              <p class="card-text">{props.description}</p>
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="link">
+                  <FaMinus size={20} />
+                </Button>
+                <Button variant="link" className="aq-text-decorate">
+                  {props.count}
+                </Button>
+                <Button variant="link">
+                  <FaPlus size={20} />
+                </Button>
+              </ButtonGroup>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="card mb-3 shadow-lg">
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={props.image} className="rounded-start" alt={props.title}/>
+                        <img src={props.image} className="rounded-start" height="200" alt={props.title}/>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
@@ -17,16 +44,12 @@ const CartPageComponent = (props) => {
 
                             </h5>
                             <p className="card-text">{props.description}</p>
-                            <ButtonGroup aria-label="Basic example">
-                                <Button variant="link"><FaMinus size={20}/></Button>
-                                <Button variant="link" className="aq-text-decorate">{props.count}</Button>
-                                <Button variant="link"><FaPlus size={20}/></Button>
-                            </ButtonGroup>
+                            
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-}
-export default CartPageComponent
+            </div> */}
+    </>
+  );
+};
+export default CartPageComponent;
--- newclient/src/PageComponents/cart/cart.js
+++ newclient/src/PageComponents/cart/cart.js
@@ -1,34 +1,40 @@
 import AquaLayout from "../../Layout/Layout";
-import { useSelector, useDispatch } from "react-redux"
-import { FaWhatsapp, FaEnvelope, FaSearch } from "react-icons/fa"
+import { useSelector, useDispatch } from "react-redux";
+import { FaWhatsapp, FaEnvelope, FaSearch } from "react-icons/fa";
 import { Button, InputGroup, Form } from "react-bootstrap";
 import CartPageComponent from "../../Components/cards/cartPageCardInCheckout";
-import AQ from "../../Assests/Default.png"
+import AQ from "../../Assests/Default.png";
 const CartComponent = () => {
-  const { cart, user } = useSelector((state) => ({ ...state }))
-  const dispatch = useDispatch()
+  const { cart, user } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
 
   const getTotal = () => {
     return cart.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
-  }
+  };
 
+  const handleAuthDialog = () => {
+    dispatch({
+      type: "SET_AUTH_DRAWER_VISIBLE",
+      payload: true,
+    });
+  };
   const showCartItems = (cart) => (
     <>
-      {
-        cart.map((r, i) => (
-          <div key={i}><CartPageComponent
+      {cart.map((r, i) => (
+        <div key={i}>
+          <CartPageComponent
             title={r.title}
             image={r.images ? r.images[0].url : AQ}
             description={r.description}
             count={r.count}
-          /></div>
-        ))
-      }
+          />
+        </div>
+      ))}
     </>
-  )
-  const showUserButtons = (user) => (
+  );
+  const showUserButtons = (user) =>
     user ? (
       <>
         <div className="mb-2">
@@ -47,13 +53,12 @@ const CartComponent = () => {
     ) : (
       <>
         <div className="d-grid gap-2 container-fluid mb-2">
-          <Button href="/login" variant="primary" size="sm">
+          <Button onClick={handleAuthDialog} size="sm">
             Login
           </Button>
         </div>
       </>
-    )
-  )
+    );
   return (
     <>
       <AquaLayout>
@@ -64,28 +69,28 @@ const CartComponent = () => {
                 <div class="card-body shadow-lg">
                   <h5 class="card-title display-5 mb-5">Cart Items</h5>
                   {!cart.length ? (
-                    <p>No Products In Cart Yet <a href="/shop">Continue Shopping</a></p>
-                  ) : showCartItems(cart)
-                  }
-
+                    <p>
+                      No Products In Cart Yet{" "}
+                      <a href="/shop">Continue Shopping</a>
+                    </p>
+                  ) : (
+                    showCartItems(cart)
+                  )}
                 </div>
-
               </div>
             </div>
             <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
               <div class="card shadow-lg mb-2">
                 <div class="card-body">
-
-
-
                   <h5 class="card-title display-5">Card title</h5>
                   <div className="mb-2">
                     <InputGroup className="mb-3">
-
-                      <Form.Control aria-label="Dollar amount (with dot and two decimal places)" placeholder="Enter Coupon" />
+                      <Form.Control
+                        aria-label="Dollar amount (with dot and two decimal places)"
+                        placeholder="Enter Coupon"
+                      />
                       <Button variant="light">Submit</Button>
                     </InputGroup>
-
                   </div>
                   <ol class="list-group">
                     <li class="list-group-item d-flex justify-content-between align-items-start">
@@ -98,7 +103,6 @@ const CartComponent = () => {
                     </li>
                   </ol>
                 </div>
-
               </div>
               <div>
                 {showUserButtons(user)}
@@ -106,11 +110,14 @@ const CartComponent = () => {
                 <div className="card shadow-lg">
                   <div className="card-body">
                     <div className="text-h6">Contact Here </div>
-                    <Button variant="link" className="text-success"><FaWhatsapp size={26} /></Button>
-                    <Button variant="link" className="text-dark"><FaEnvelope size={25} /></Button>
+                    <Button variant="link" className="text-success">
+                      <FaWhatsapp size={26} />
+                    </Button>
+                    <Button variant="link" className="text-dark">
+                      <FaEnvelope size={25} />
+                    </Button>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
