c83f46459db4cf2011f6b039ad3984f5ee6d8d77
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-05-05 01:02:05 +0530

    compare

--- newclient/src/PageComponents/Compare/Compare.js
+++ newclient/src/PageComponents/Compare/Compare.js
@@ -1,12 +1,40 @@
+import CompareCommonCard from "../../Components/cards/CompareCommonCard";
 import AquaLayout from "../../Layout/Layout";
 import UserCartAndWish from "./userCartAndWishListTab";
-
+import { useSelector, useDispatch } from "react-redux";
 const ComparePage = () => {
+  const dispatch = useDispatch();
+  const { user, cart } = useSelector((state) => ({ ...state }));
+
+  const CartProductsLoad = () => {
+    return (
+      <>
+        <h1>Cart Products Load</h1>
+        <hr />
+        {cart.map((r, i) => (
+          <div>
+            <CompareCommonCard title={r.title} />
+          </div>
+        ))}
+      </>
+    );
+  };
+  const WishListProductsLoad = () => {
+    return (
+      <>
+        <h1>WishList Products Load</h1>
+        <hr />
+      </>
+    );
+  };
   return (
     <>
       <AquaLayout>
         <div className="container">
-          <UserCartAndWish />
+          <UserCartAndWish
+            cart={<CartProductsLoad />}
+            wishlist={<WishListProductsLoad />}
+          />
           <h1 className="text-center display-2">Compare Your Products</h1>
         </div>
       </AquaLayout>
--- newclient/src/PageComponents/Compare/userCartAndWishListTab.js
+++ newclient/src/PageComponents/Compare/userCartAndWishListTab.js
@@ -2,7 +2,7 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import DefaultCardWithShadow from "../../Components/cards/defaultCardMatterShadow";
 
-const UserCartAndWish = () => {
+const UserCartAndWish = (props) => {
   return (
     <Tabs
       defaultActiveKey="profile"
@@ -11,19 +11,13 @@ const UserCartAndWish = () => {
       fill
     >
       <Tab eventKey="home" title="Cart">
-        <DefaultCardWithShadow>
-        <h1>Tab1</h1>
-        </DefaultCardWithShadow>
+        <DefaultCardWithShadow>{props.cart}</DefaultCardWithShadow>
       </Tab>
       <Tab eventKey="profile" title="WishList">
-        <DefaultCardWithShadow>
-        <h1>Tab2</h1>
-        </DefaultCardWithShadow>
+        <DefaultCardWithShadow>{props.wishlist}</DefaultCardWithShadow>
       </Tab>
       <Tab eventKey="longer-tab" title="Trending">
-      <DefaultCardWithShadow>
-        <h1>Tab3</h1>
-        </DefaultCardWithShadow>
+        <DefaultCardWithShadow>{props.Trending}</DefaultCardWithShadow>
       </Tab>
     </Tabs>
   );
